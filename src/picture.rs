2fb6ffb9d0dd2289a53a5188d4427be7d4f7e8ca
Harry Wixley <hwixley1@gmail.com>
2024-10-19 21:01:21 +0100

    GPT-commit: Refactored image processing print statements.
    Refactored image parsing print statements for clarity.

--- src/picture.rs
+++ src/picture.rs
@@ -21,23 +21,23 @@ impl MyPicture {
     }
 
     pub fn load_gray_img(&self, nwidth: u32) -> GrayImage {
-        println!("Parsing image into grayscale with width {}...", nwidth);
-
         let gray: ImageBuffer<image::Luma<u8>, Vec<u8>> = DynamicImage::ImageRgba8(MyPicture::load_img(self)).into_luma8();
         let (width, height): (u32, u32) = gray.dimensions();
         let scale: f32 = (width as f32)/(nwidth as f32);
         let new_height: f32 = (height as f32)/scale;
 
+        println!("Parsing image into grayscale with width {}...", nwidth);
+
         return imageops::resize(&gray, nwidth as u32, new_height as u32, imageops::FilterType::CatmullRom)
     }
 
     pub fn to_ascii(&self, twidth: u32) -> String {
-        println!("Parsing image into ASCII string of width {}...", twidth);
-
         let charset: String = " .:-=+*#%@".to_string();
         let img: ImageBuffer<image::Luma<u8>, Vec<u8>> = MyPicture::load_gray_img(self, twidth);
         let (width, _): (u32, u32) = img.dimensions();
 
+        println!("Parsing image into ASCII string of width {}...", twidth);
+        
         let mut ascii_out: String = "".to_string();
         for pixel in img.enumerate_pixels() {
             let pixel_val: usize = pixel.2.channels()[0] as usize;
